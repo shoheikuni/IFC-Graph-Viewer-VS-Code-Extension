e03c63ec9897a2112b3a5c03255f84f6d34094cf
Shohei Kunimatsu <shoheikuni@gmail.com>
2026-04-27 13:05:06 +0900

    IFCファイル選択方法。 1. エクスプローラでIFCファイルを右クリック 2. IFCファイルを開いた状態でエディタ右クリック 3. IFCファイルを開いた状態でコマンドパレット

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,37 @@
-import { commands, ExtensionContext } from "vscode";
+import { commands, ExtensionContext, window, Uri, workspace } from "vscode";
 import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 
 export function activate(context: ExtensionContext) {
-  const openViewerCommand = commands.registerCommand("ifc-graph-viewer.openViewer", () => {
+  const openViewerCommand = commands.registerCommand("ifc-graph-viewer.openViewer",
+						     async (uri?: Uri) => {
 
     let ifcText: string | undefined;
 
-    ifcText = ""; // TODO: Webviewに渡すIFC文字列
+    // エクスプローラ右クリック（ファイルuriが渡される）
+    if (uri) {
+      if (!uri.fsPath.toLowerCase().endsWith(".ifc")) {
+        window.showErrorMessage("IFCファイルではありません");
+        return;
+      }
+      const bytes = await workspace.fs.readFile(uri);
+      ifcText = bytes.toString();
+    }
+    // コマンドパレットまたはエディタ右クリック
+    else {
+      const editor = window.activeTextEditor;
+      if (!editor) {
+        window.showErrorMessage("IFCファイルを開いてください");
+	      return;
+      }
+
+      const document = editor.document;
+      if (!document.fileName.toLowerCase().endsWith(".ifc")) {
+        window.showErrorMessage("IFCファイルではありません");
+	      return;
+      }
+
+      ifcText = document.getText();
+    }
 
     // Webview起動
     HelloWorldPanel.render(context.extensionUri, ifcText);
